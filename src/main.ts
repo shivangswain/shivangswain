b9a2db74544d2c4a6e922e3d4155d764120f52be
Shivang P Swain <shivangswain@outlook.com>
2023-07-11 01:32:43 +0530

    Major refactor
    - Simplified a lot of code
    - Better rewritten comments

--- src/main.ts
+++ src/main.ts
@@ -8,29 +8,27 @@ import { toggleDarkMode } from './components/switcher';
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
-<div class='content'>
-    <div class='main-text'>
-        <h1>
-            Hi, I'm Shivang<span class='slide-out'> P Swain</span>.
-        </h1>
-        <p>I'm a 23 year old cybersecurity analyst working in the Technology Risk division at Goldman
-            Sachs.
-        </p>
-        <div class='links'>
-            <a href='/github' target='_blank' aria-label='GitHub'><i class='fa-brands fa-github'></i></a>
-            <a href='/linkedin' target='_blank' aria-label='LinkedIn'><i class='fa-brands fa-linkedin-in'></i></a>
-            <a href='/instagram' target='_blank' aria-label='Instagram'><i class='fa-brands fa-instagram'></i></a>
+    <div class='content'>
+        <div class='main-text'>
+            <h1>
+                Hi, I'm Shivang<span class='slide-out'> P Swain</span>.
+            </h1>
+            <p>I'm a 23 year old cybersecurity analyst working in the Technology Risk division at Goldman Sachs.</p>
+            <div class='links'>
+                <a href='/github' target='_blank' aria-label='GitHub'><i class='fa-brands fa-github'></i></a>
+                <a href='/linkedin' target='_blank' aria-label='LinkedIn'><i class='fa-brands fa-linkedin-in'></i></a>
+                <a href='/instagram' target='_blank' aria-label='Instagram'><i class='fa-brands fa-instagram'></i></a>
+            </div>
         </div>
+        <button id='toggle' class='toggle' aria-label='Light/Dark Mode toggle' type='button'>
+            <span id='moon-icon' class='material-symbols-outlined'>dark_mode</span>
+            <span id='sun-icon' class='material-symbols-outlined'>light_mode</span>
+        </button>
     </div>
-    <button id='toggle' class='toggle' aria-label='Light/Dark Mode toggle' type='button'>
-        <span id='moon-icon' class='material-symbols-outlined'>dark_mode</span>
-        <span id='sun-icon' class='material-symbols-outlined'>light_mode</span>
-    </button>
-</div>
-`
+`;
 
 // Select the toggle button
 const toggleButton = document.querySelector<HTMLButtonElement>('#toggle')!;
 
-// Call toggleDarkMode function
-toggleDarkMode(toggleButton)
+// Call toggleDarkMode function to enable Light/Dark mode switch
+toggleDarkMode(toggleButton);
