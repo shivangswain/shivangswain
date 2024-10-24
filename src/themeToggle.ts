958ff3b8b1074aba5de53ff33b500255fc25075d
shivangswain <me@shivangswain.com>
2024-10-25 01:21:35 +0530

    Hover fixes, theme toggle fixes, and defer & swap Google Fonts

--- src/themeToggle.ts
+++ src/themeToggle.ts
@@ -7,11 +7,14 @@ type Theme = "light" | "dark" | "root";
  * Sets up the theme toggle functionality
  */
 export function setupThemeToggle() {
-  const themeColor = document.querySelector('meta[name="theme-color"]') as HTMLMetaElement;
+  const themeColor = document.querySelector(
+    'meta[name="theme-color"]'
+  ) as HTMLMetaElement;
   const themeToggle = document.getElementById(
     "theme-toggle"
   ) as HTMLButtonElement;
   const themeIcon = themeToggle.querySelector("i") as HTMLElement;
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
   // Function to get the next theme in the cycle
   const getNextTheme = (currentTheme: Theme): Theme => {
@@ -33,17 +36,17 @@ export function setupThemeToggle() {
     // Update icon based on theme
     switch (theme) {
       case "light":
-        themeColor.setAttribute('content', "#d7d7d7");
+        themeColor.setAttribute("content", "#d7d7d7");
         themeIcon.textContent = "light_mode";
         themeIcon.className = "material-symbols-rounded light";
         break;
       case "dark":
-        themeColor.setAttribute('content', "#181818");
+        themeColor.setAttribute("content", "#181818");
         themeIcon.textContent = "dark_mode";
         themeIcon.className = "material-symbols-rounded dark";
         break;
       case "root":
-        themeColor.setAttribute('content', "#1b2e8b");
+        themeColor.setAttribute("content", "#1b2e8b");
         themeIcon.textContent = "star";
         themeIcon.className = "material-symbols-rounded star";
         break;
@@ -54,14 +57,21 @@ export function setupThemeToggle() {
   };
 
   // Check for saved user preference, if any, on load
-  const savedTheme = (localStorage.getItem("theme") as Theme) || "root";
+  const savedTheme =
+    (localStorage.getItem("theme") as Theme) || prefersDarkScheme ? "dark" : "light";
   updateTheme(savedTheme);
 
   // Listen for clicks on the toggle button
   themeToggle.addEventListener("click", () => {
-    const currentTheme = (localStorage.getItem("theme") as Theme) || "root";
+    const currentTheme =
+      (localStorage.getItem("theme") as Theme) || "root";
     const nextTheme = getNextTheme(currentTheme);
     updateTheme(nextTheme);
+  });
+
+  // Listen for changes in the OS-level color scheme preference
+  prefersDarkScheme.addEventListener("change", (e) => {
+    updateTheme(e.matches ? "dark" : "light");
   });
 }
 
